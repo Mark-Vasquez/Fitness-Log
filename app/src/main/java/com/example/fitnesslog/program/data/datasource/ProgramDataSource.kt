869588a43163d173621fe0395c7762bb7cd73e97
Mark Vasquez <markjasonvasquez1@gmail.com>
2023-09-07 19:11:43 -0700

    Make fragment container dynamic
    Stretch fragment container down to parent when bottomNav is invisible

--- app/src/main/java/com/example/fitnesslog/program/data/datasource/ProgramDataSource.kt
+++ app/src/main/java/com/example/fitnesslog/program/data/datasource/ProgramDataSource.kt
@@ -6,7 +6,7 @@ class ProgramDataSource {
     fun loadPrograms(): List<Program> {
         return listOf(
             Program("Push Pull Legs", 6),
-            Program("Arnold Bodybuilding Suck My Hoopla", 5),
+            Program("Arnold Bodybuilding", 5),
             Program("5x5 StrongLift", 3),
         )
     }
