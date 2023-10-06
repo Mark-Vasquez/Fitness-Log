7b7c2dc7745e29b64f498796e447608a1032c158
Mark Vasquez <markjasonvasquez1@gmail.com>
2023-10-05 22:29:08 -0700

    Edit program use case

---
+++ app/src/main/java/com/example/fitnesslog/program/domain/use_case/EditProgramUseCase.kt
@@ -0,0 +1,13 @@
+package com.example.fitnesslog.program.domain.use_case
+
+import com.example.fitnesslog.program.data.entity.Program
+import com.example.fitnesslog.program.domain.repository.ProgramRepository
+
+class EditProgramUseCase(
+    private val programRepository: ProgramRepository
+) {
+
+    suspend operator fun invoke(program: Program) {
+        programRepository.updateProgram(program)
+    }
+}
