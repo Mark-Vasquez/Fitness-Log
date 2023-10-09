3666ca1bcd4bd880ab8241063358b14e6d47f0a0
Mark Vasquez <markjasonvasquez1@gmail.com>
2023-10-09 04:53:19 -0700

    Make a ViewModelFactory helper

--- app/src/main/java/com/example/fitnesslog/core/ui/ViewModelFactoryHelper.kt
+++ app/src/main/java/com/example/fitnesslog/core/ui/ViewModelFactoryHelper.kt
@@ -1,2 +1,15 @@
 package com.example.fitnesslog.core.ui
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+inline fun <reified VM : ViewModel> viewModelFactory(crossinline creator: () -> VM): ViewModelProvider.Factory {
+    return object : ViewModelProvider.Factory {
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(VM::class.java)) {
+                return creator() as T
+            }
+            throw IllegalArgumentException("ViewModel passed in the Provider does not match ViewModel configured in the Factory")
+        }
+    }
+}
