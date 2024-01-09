3def2fecf44acea8d70da99bc2edb8abee131f0a
Mark Vasquez <markjasonvasquez1@gmail.com>
2024-01-09 10:37:03 -0800

    Fix programsViewModel to properly implement selecting a program

--- app/src/main/java/com/example/fitnesslog/shared/di/SharedModule.kt
+++ app/src/main/java/com/example/fitnesslog/shared/di/SharedModule.kt
@@ -1,8 +1,7 @@
 package com.example.fitnesslog.shared.di
 
 import com.example.fitnesslog.FitnessLogApp.Companion.programModule
-import com.example.fitnesslog.program.domain.repository.ProgramRepository
-import com.example.fitnesslog.program.domain.use_case.SelectProgram
+import com.example.fitnesslog.shared.domain.use_case.GetSelectedProgram
 import com.example.fitnesslog.shared.domain.use_case.SharedUseCases
 
 interface SharedModule {
@@ -11,11 +10,9 @@ interface SharedModule {
 
 class SharedModuleImpl() : SharedModule {
 
-    private val programRepository: ProgramRepository = programModule.programRepository
-
     override val sharedUseCases: SharedUseCases by lazy {
         SharedUseCases(
-            selectProgram = SelectProgram(programRepository)
+            getSelectedProgram = GetSelectedProgram(programModule.programRepository)
         )
     }
 
